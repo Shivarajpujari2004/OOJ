ad0809eef23236ecd0c27684b7843fd128d180c9
Shivarajpujari2004 <153583611+Shivarajpujari2004@users.noreply.github.com>
2024-02-06 21:03:10 +0530

    Add files via upload

---
+++ LAB-8-1BM22CS259/Tmain.java
@@ -0,0 +1,46 @@
+class CS extends Thread
+{
+public void run()
+{
+for(int i=1; i<=20; i++){
+	try{
+		
+		System.out.println("BMS College of Engineering" + i);
+		Thread.sleep(10000);
+	}
+	catch(InterruptedException e){
+		System.out.println("thread error");
+	}
+}
+}
+}
+
+class IS extends Thread
+{
+public void run()
+{
+for(int i=1; i<=20; i++){
+	try{
+		
+		System.out.println("Computer Science " + i);
+		Thread.sleep(2000);
+	}
+	catch(InterruptedException e){
+		System.out.println("thread error");
+	}
+}
+}
+}
+
+public class Tmain {
+public static void main(String args[])
+{
+CS c1 = new CS();
+c1.start();
+
+IS i1 = new IS();
+i1.start();
+System.out.println("NAME:SHIVARAJ KALLAPPA PUJARI");
+System.out.println("USN :1BM22CS259");
+}
+}
